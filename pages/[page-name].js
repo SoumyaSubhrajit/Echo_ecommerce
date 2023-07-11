253ed665ad8012c57f9926b7797e810e37dfeacc
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-07-11 18:37:58 +0530

    Changing the tittle.

---
+++ pages/[page-name].js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function MyPage() {
+  return (
+    <div>
+      <Head>
+        <title>Echo Store</title>
+      </Head>
+      <h1>This is my new page</h1>
+    </div>
+  );
+}
+
+export default MyPage;
