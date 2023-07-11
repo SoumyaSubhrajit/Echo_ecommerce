ff2d8f9ab0d08aa8df97edbc6928da0764f9c1da
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-07-11 18:53:39 +0530

    Adding some more chaneges.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import '../styles/globals.css'
-import {Layout} from '../components'
-import { StateContext} from '../context/StateContext'
-import { Toaster} from 'react-hot-toast'
+import { Layout } from '../components'
+import { StateContext } from '../context/StateContext'
+import { Toaster } from 'react-hot-toast'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return(
+  return (
+    <>
+      <Head>
+        <title>Echo Store</title>
+      </Head>
+
       <StateContext>
-      <Layout>
-          <Toaster/>
-        <Component {...pageProps} />
-      </Layout>
+        <Layout>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
       </StateContext>
+    </>
   )
 }
 
 export default MyApp
+
+
